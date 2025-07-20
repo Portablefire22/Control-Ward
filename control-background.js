5497f41bdc92873a022d509d16f7c1007f4ebb55
Portablefire22 <lilith@kitten.rs>
2025-07-21 00:19:01 +0100

    Streamers are monitored and user is notified.
    Twitch API is queried for currently hard-coded streamer list. If streamers are found then individual notifications are sent out.
    
    Notifications are tracked and will redirect when clicked on, these are deleted after 1 minute to prevent any memory issues with the object accumulating too much. Should not be a concern on modern hardware though.
    
    Authentication tokens are gathered from storage and query OAuth if required.

--- control-background.js
+++ control-background.js
@@ -1,39 +1,168 @@
 const clientId = "kgl6x6l9jun0frygndc06y0vqrofy6";
 const redirectUri = browser.identity.getRedirectURL();
 
+const extensionName = "Control Ward";
+
 const twitch = {
     isActive: false,
     token: "",
+    streamers: ["ratirl", "fence", "r0se"],
+};
+
+const monitoredStreams = {};
+const notifications = {};
+
+function log(string) {
+    console.log(`[${extensionName}] ${string}`);
 }
 
-export function getToken() {
-    browser.notifications.create({
-                type: "basic",
-                iconUrl: browser.runtime.getURL(""),
-                title: "Hello",
-                message: "meow"
-            });
-   /*
-    console.log(auth);*/
+async function getToken() {
+    let storageResult = await browser.storage.sync.get("twitch");
+    if (storageResult != null && storageResult.twitch) {
+        let res = storageResult.twitch;
+        if (res != null && res.token.length > 0) {
+            twitch.isActive = true;
+            twitch.token = res.token;
+            log(`Found token in storage`);
+            return twitch.token;
+        }
+    } 
+    log(`Getting token via Oauth`);
+    return await auth();
 }
 
-function auth() {
-    let auth = browser.identity.launchWebAuthFlow({
+function extractToken(redirect) {
+    let auth = redirect.match(/(?<==)(.*?)(?=&)/);
+    if (!auth || auth.length < 1) {
+        return null;
+    }
+    return auth[0]
+}
+
+function setToken(token) {
+    twitch.isActive = true;
+    twitch.token = token;
+    browser.storage.sync.set({"twitch": twitch});
+}
+
+async function auth() {
+    let val = await browser.identity.launchWebAuthFlow({
         url: "https://id.twitch.tv/oauth2/authorize" +
             `?client_id=${clientId}` +
             "&response_type=token" +
             `&redirect_uri=${redirectUri}`,
             interactive: true
-    }).then(val => {
-         browser.notifications.create({
-                type: "basic",
-                iconUrl: browser.runtime.getURL(""),
-                title: "Hello",
-                message: `${val}`
-            });
-    });   
+    })
+    let params = new URL(val);
+    let token = params.hash;
+    console.log(token);
+    let extracted = extractToken(token);
+    setToken(`${extracted}`);
+    return extracted;
 }
 
-browser.runtime.onMessage.addListener(() => {
-    auth();
+function notify(stream) {
+    const thumbnail_url = stream.thumbnail_url.replace("{width}x{height}", "178x100");
+    let id = crypto.randomUUID();
+    notifications[id] = stream.user_login;
+    browser.notifications.create(id, {
+        type: "basic",
+        title: `${stream.user_name} is Streaming - ${stream.game_name}`,
+        iconUrl: thumbnail_url,
+        message: `${stream.title}`
+    });
+
+    browser.notifications.onClicked.addListener((notificationId) => {
+        const streamer = notifications[notificationId];
+        if (!streamer) return;
+        log(`https://twitch.tv/${streamer}`);
+        browser.tabs.create({
+            url: `https://twitch.tv/${streamer}`,
+        });
+        delete notifications[notificationId];
+    });
+
+    // Incase of some weird memory consumption, lets just delete the notifications if they haven't been clicked for a minute. 
+    setTimeout(() => {
+        // I'm going to assume that Javascript just handles this perfectly
+        delete notifications[notificationId];
+    }, 60000);
+}
+
+function startMonitoring() {
+    log("Starting...");
+    log(`Monitoring: ${twitch.streamers}`);
+    monitoring();
+}
+
+async function monitoring() {
+    const interval = setInterval(() => {
+        if (!twitch.isActive) clearInterval(interval);
+        log("Searching for streams");
+        searchStreamers(twitch.streamers).then(liveStreams => {
+            for (let stream of liveStreams) {
+                notify(stream);
+            }
+        });
+    }, 10000);
+}
+
+/// Returns null on error or if no streams :)
+async function searchStreamers(streamerNames) {
+    if (streamerNames.length == 0) {
+        return null;
+    }
+    let uri = "https://api.twitch.tv/helix/streams?";
+    for (let streamer of streamerNames) {
+        uri += `user_login=${streamer}&`;
+    }
+    const resp = await fetch(uri, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${twitch.token}`,
+            "Client-Id": `${clientId}`
+        },
+    });
+    if (!resp.ok) return null;
+    const json = await resp.json();
+    if (!json.data || json.data.length == 0) return null;
+    let streams = [];
+    // Iterate through all streams we got
+    for (let stream of json.data) {
+        // We don't want to notify constantly so let's check if we have already notified for this stream
+        let streamCheck = monitoredStreams[stream.user_login];
+        if (streamCheck && streamCheck == stream.id) {
+            continue;
+        }
+        streams.push(stream);
+        monitoredStreams[stream.user_login] = stream.id;
+    }
+    return streams;
+}
+
+async function startup() {
+  for (let i = 0; i < 2; i++) {
+        if (twitch.isActive) {
+            log(`${twitch.token}`);
+            startMonitoring();
+            break;
+        }
+        await getToken();
+    } 
+}
+
+browser.runtime.onMessage.addListener(async () => {
+    if (twitch.isActive) {
+        log("Already monitoring");
+        return;
+    }
+    await startup();
+});
+
+
+
+// Try to monitor, if twitch token isn't set then try to get 
+// authorisation 
+browser.runtime.onStartup.addListener(async () => {
+    await startup();
 });
